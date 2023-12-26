169321b894b98b5d9380589f4068ee14d4a3a466
a <a@pop-os.localdomain>
2023-12-26 15:10:36 +0100

    Equivalent expressions

---
+++ ch2/2.11_chapterExercises/2_equivalentExpressions.hs
@@ -0,0 +1,26 @@
+{-
+Which of the following pairs of expressions will return the same
+result when evaluated? Try to reason them out in your head by
+reading the code and then enter them into the REPL to check your
+work:
+
+1.  1 + 1
+    2
+    Same
+
+2.  10 ^ 2
+    10 + 9 * 10
+    Yes
+
+3.  400 - 37
+    (-) 37 400
+    No
+
+4.  100 `div` 3
+    100 / 3
+    No, (/) returns a Fractional, div returns an Integral
+
+5.  2 * 5 + 18
+    2 * (5 + 18)
+    No
+-}
